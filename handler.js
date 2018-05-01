b1c9ca1d2fd77f3c6d71657e439e5285aad1563e
ogontaro <ogontaro@gmail.com>
2018-05-02 00:04:18 +0900

    typo

--- handler.js
+++ handler.js
@@ -7,19 +7,18 @@ module.exports.hello = (event, context, callback) => {
   const conversationId = process.env.SLACK_CHANNEL_ID;
   const body = JSON.parse(event["body"]);
   const challenge_message = body["challenge"];
-  const type = body["type"];
+  const type = body["event"]["type"];
 
+  console.log(body);
   switch (type) {
     case "channel_rename":
-      const channel = "#"+body["channel"]["name"];
+      const channel = "#"+body["event"]["channel"]["name"];
       const random_messages = [
         `おほ～♪ 新しいチャンネル ${channel} ができたよー`,
         `${channel} ができたか。ここは聖地と呼びたい`,
         `${channel} ができたって？ こいつぁすげぇや！`,
         `${channel} ができたよ！シロちょっとチャンネル通知の才能あるかも♪`
       ];
-      console.log(random_messages.length);
-      console.log( Math.floor(Math.random() * random_messages.length) );
       let text = random_messages[Math.floor(Math.random() * random_messages.length)];
       web.chat.postMessage({ channel: conversationId, text: text, link_names: 1 })
         .then((res) => {
