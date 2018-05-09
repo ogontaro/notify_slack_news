d81de93357cd65df1c183387643362a052e57a17
ogontaro <ogontaro@gmail.com>
2018-05-10 00:16:56 +0900

    fix lint

--- app/i18next.js
+++ app/i18next.js
@@ -1,10 +1,12 @@
+/* eslint-disable import/no-dynamic-require */
 import i18next from 'i18next';
 
+const locale = require(`../config/locales/${process.env.AI_NAME}.yml`);
 const I18n = i18next
   .init({
     fallbackLng: 'ja',
     debug: true,
-    resources: require(`../config/locales/${process.env.AI_NAME}.yml`)
+    resources: locale,
   });
 
 export default I18n;
