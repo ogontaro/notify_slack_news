405d48c1090bdb2050564fd5ed70b310e321adf8
ogontaro <ogontaro@gmail.com>
2018-05-09 20:51:35 +0900

    message file load from yaml

--- app/i18next.js
+++ app/i18next.js
@@ -4,23 +4,7 @@ const I18n = i18next
   .init({
     fallbackLng: 'ja',
     debug: true,
-    resources: {
-      ja: {
-        translation: {
-          channel_created:
-            [
-              'おほ～♪新しい #{{name}} を発見しましたぁ♪',
-              '#{{name}} ができたの！？ ここは聖地と呼びたい！',
-              '#{{name}} ができたって？ こいつぁすげぇや！',
-            ],
-          emoji_changed:
-            {
-              add: '新しい絵文字が追加されたよ→ :{{name}}: みんな使ってみて！',
-              remove: '残念ながら :{{name}}: の絵文字はおなくなりになりました…',
-            },
-        },
-      },
-    },
+    resources: require("../config/locales/shiro.yml")
   });
 
 export default I18n;
